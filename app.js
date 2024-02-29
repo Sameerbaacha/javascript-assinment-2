56d276b741fdb5ca1ecf349976b5f91ecbc4a0c8
Sameerbaacha <sameeris6512@gmail.com>
2024-03-01 00:43:31 +0500

    document ko app.js m bhja h

---
+++ app.js
@@ -0,0 +1,60 @@
+
+{/* /* // =========================== chapter #04 ================================
+
+// ====Assignment#02 ====
+// ====Question #01 =====
+// var a, b, c;
+
+// ====Assignment#02 ====
+// ====Question #02 =====
+
+//*************5 legal variables names************
+
+// 1: Using letters, digits, underscores, and dollar signs:
+// var myVariable = 10;
+
+// 2: Starting with letters, underscores, or dollar signs
+// var _variableName = "Hello";
+
+// 3: Using camelCase convention:
+// var camelCaseVariable = 20;
+
+// 4: Declaring without assignment:
+// var Login;
+
+// 5: Underscore in variable name:
+// var user_name = "sameer123";
+
+//*************5 illegal variables names************
+
+// 1: Starting with a number:
+// var 1illegal = "This is not allowed";
+
+// 2: Using spaces:
+// var illegal variable = "Spaces are not allowed";
+
+// 3: Using reserved keywords:
+// var break = "This will cause an error";
+
+// Starting with special characters(except underscore _ or dollar sign $):
+// var @illegal = "Special characters are not allowed";
+
+// Re - declaring with var:
+// var x = 5;
+// var x = 10; // Error: Identifier 'x' has already been declared
+
+====Assignment#02 ====
+====Question #02 ===== */}
+var heading = "<h1>Rules of naming Js Variables</h1></br>"
+document.write(heading)
+document.write("Variable names must start with a letter, an underscore ( _ ) or a dollar sign ( $ ).</br>")
+document.write("Variable names cannot contain spaces.</br>")
+document.write("Variables cannot be the same as reserved keywords such as if or const.</br>")
+document.write("By convention, JavaScript variable names are written in camelCase.</br>")
+
+
+// =========================== chapter #04 ================================
+
+// ====Assignment#02 ====
+// ====Question #01 =====
+
