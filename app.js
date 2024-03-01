61eb66be5226eaf5f3cb93341a69067ae24a9f6b
Sameerbaacha <sameeris6512@gmail.com>
2024-03-01 11:08:02 +0500

    change in app.js

--- app.js
+++ app.js
@@ -1,60 +1,314 @@
 
-{/* /* // =========================== chapter #04 ================================
+// =========================== chapter #04 ================================
 
 // ====Assignment#02 ====
+// === chapter #04 ===
 // ====Question #01 =====
 // var a, b, c;
 
 // ====Assignment#02 ====
+// === chapter #04 ===
 // ====Question #02 =====
 
 //*************5 legal variables names************
 
 // 1: Using letters, digits, underscores, and dollar signs:
-// var myVariable = 10;
+// var myVariable;
 
 // 2: Starting with letters, underscores, or dollar signs
-// var _variableName = "Hello";
+// var _variableName;
 
 // 3: Using camelCase convention:
-// var camelCaseVariable = 20;
+// var camelCaseVariable;
 
 // 4: Declaring without assignment:
 // var Login;
 
 // 5: Underscore in variable name:
-// var user_name = "sameer123";
+// var user_name;
 
 //*************5 illegal variables names************
 
 // 1: Starting with a number:
-// var 1illegal = "This is not allowed";
+// var 1illegal;
 
 // 2: Using spaces:
-// var illegal variable = "Spaces are not allowed";
+// var illegal variable;
 
 // 3: Using reserved keywords:
-// var break = "This will cause an error";
+// var break;
 
 // Starting with special characters(except underscore _ or dollar sign $):
-// var @illegal = "Special characters are not allowed";
+// var @illegal;
 
 // Re - declaring with var:
-// var x = 5;
-// var x = 10; // Error: Identifier 'x' has already been declared
-
-====Assignment#02 ====
-====Question #02 ===== */}
-var heading = "<h1>Rules of naming Js Variables</h1></br>"
-document.write(heading)
-document.write("Variable names must start with a letter, an underscore ( _ ) or a dollar sign ( $ ).</br>")
-document.write("Variable names cannot contain spaces.</br>")
-document.write("Variables cannot be the same as reserved keywords such as if or const.</br>")
-document.write("By convention, JavaScript variable names are written in camelCase.</br>")
-
-
-// =========================== chapter #04 ================================
+// var x;
+// var x; // Error: Identifier 'x' has already been declared
 
 // ====Assignment#02 ====
-// ====Question #01 =====
+// === chapter #04 ===
+// ====Question #03 =====
+// var heading = "<h1>Rules of naming Js Variables</h1></br>"
+// document.write(heading)
+// document.write("Variable names must start with a letter, an underscore ( _ ) or a dollar sign ( $ ).</br>")
+// document.write("Variable names cannot contain spaces.</br>")
+// document.write("Variables cannot be the same as reserved keywords such as if or const.</br>")
+// document.write("By convention, JavaScript variable names are written in camelCase.</br>")
 
+
+// =========================== chapter #05 ================================
+
+// ====Assignment#02 ====
+// =====chapter #05=====
+// ====Question #01 =====
+// var num1=3;
+// var num2=5;
+// var sum=num1+num2;
+// document.write("Sum of 3 and 5 is "+sum);
+
+
+// ====Assignment#02 ====
+// =====chapter #05=====
+// ====Question #02 =====
+// var num1=3;
+// var num2=5;
+// var sub=num1-num2;
+// document.write("Subtraction of 3 and 5 is "+sub);
+
+// var num1=3;
+// var num2=5;
+// var multiple=num1*num2;
+// document.write("multiplication of 3 and 5 is "+multiple);
+
+// var num1=3;
+// var num2=5;
+// var div=num1*num2;
+// document.write("division of 3 and 5 is "+div);
+
+// var num1=3;
+// var num2=5;
+// var modu=num1%num2;
+// document.write("modulus of 3 and 5 is "+modu);
+
+
+
+// ====Assignment#02 ====
+// =====chapter #05=====
+// ====Question #03 =====
+// ======== (a) =======
+// var myVariable;
+// ======== (b) =======
+// document.write("value after variable declaration is "+myVariable+"</br>");
+// ======== (c) =======
+// var number=5;       
+
+// ======== (d) =======
+// document.write("Initial value is: "+number+"</br>");
+
+// ======== (e) =======
+
+// ++number;
+
+// ======== (f) =======
+// document.write("Value After Increamnt is :"+number+"</br>");
+
+// ======== (g) =======
+// var sum=number+7;
+
+// ======== (h) =======
+// document.write("value after addition is: "+sum+"</br>")
+
+// ======== (i) =======
+// --sum;
+// ======== (j) =======
+// document.write("value after decreament is :"+sum+"</br>");
+
+// ======== (k) =======
+// var remain=sum%3;
+
+// ======== (l) =======
+// document.write("The remainder is: "+remain+"</br>")
+
+
+// ====Assignment#02 ====
+// =====chapter #05=====
+// ====Question #04 =====
+
+// var costOfMovie="600";
+// var totalCostOfMovie= costOfMovie*"5";
+// document.write("Total cost to buy 5 tickets to a movie is: "+totalCostOfMovie+"PKR");
+
+
+// ====Assignment#02 ====
+// =====chapter #05=====
+// ====Question #05 =====
+// document.write("<h1>Table of 4</h1>")
+// var four="4";
+// num="1";
+// var product=four*num;
+// document.write(four+"x"+num+"="+product+"</br>");
+
+// num++;
+// var product=four*num;
+// document.write(four+"x"+num+"="+product+"</br>");
+
+// num++;
+// var product=four*num;
+// document.write(four+"x"+num+"="+product+"</br>");
+
+// num++;
+// var product=four*num;
+// document.write(four+"x"+num+"="+product+"</br>");
+
+// num++;
+// var product=four*num;
+// document.write(four+"x"+num+"="+product+"</br>");
+
+// num++;
+// var product=four*num;
+// document.write(four+"x"+num+"="+product+"</br>");
+
+// num++;
+// var product=four*num;
+// document.write(four+"x"+num+"="+product+"</br>");
+
+// num++;
+// var product=four*num;
+// document.write(four+"x"+num+"="+product+"</br>");
+// num++;
+// var product=four*num;
+// document.write(four+"x"+num+"="+product+"</br>");
+
+// num++;
+// var product=four*num;
+// document.write(four+"x"+num+"="+product+"</br>");
+
+
+
+// ====Assignment#02 ====
+// =====chapter #05=====
+// ====Question #06 =====
+// var Celsius="25";
+// var fahrenheit=(Celsius*9/5)+32;
+// document.write(Celsius+"'C is "+fahrenheit+"'F"+"</br>");
+
+
+// var fahrenheit="70";
+// var Celsius=(fahrenheit-32)*5/9;
+// document.write(fahrenheit+"'C is "+Celsius+"'F");
+
+
+
+// ====Assignment#02 ====
+// =====chapter #05=====
+// ====Question #07 =====
+// document.write("<h1>SHOPPING STORE</h1>")
+// var priceOfItem1="650";
+// var priceOfItem2="100";
+// var oderQuantityOfItem1="3";
+// var oderQuantityOfItem2="7";
+// var shippingCharges="100";
+
+// var totalCostOfOrder=(priceOfItem1*oderQuantityOfItem1)+(priceOfItem2*oderQuantityOfItem2)+shippingCharges;
+// document.write("Price of item1 is "+priceOfItem1+"</br>"+"Oder Quantity of item1 is "+oderQuantityOfItem1+"</br>"+"Price of item2 is "+priceOfItem2+"</br>"+"Oder Quantity of item2 is "+oderQuantityOfItem1+"</br>"+"Shipping Charges is "+shippingCharges+"</br>"+"</br>")
+// document.write("Total cost of your order is "+totalCostOfOrder);
+
+
+
+// ====Assignment#02 ====
+// =====chapter #05=====
+// ====Question #08 =====
+// document.write("<h1>MARKSHEET</h1>")
+// var totalMarks="980";
+// var obtMarks="804";
+// var percentage=obtMarks/totalMarks*100;
+// document.write("Total Marks:980"+"</br>"+"Marks Obtained:804"+"</br>")
+// document.write("Percentage:"+percentage+"%");
+
+
+// ====Assignment#02 ====
+// =====chapter #05=====
+// ====Question #09 =====
+// document.write("<h1>Currency IN PKR</h1>")
+// var usDollar="10";
+// var saudiRiyal="25";
+// var rupeesInPkr=(usDollar*104.80)+(saudiRiyal*28);
+// document.write("Total Rupees in PKR: "+"Rs "+rupeesInPkr);
+
+
+
+// ====Assignment#02 ====
+// =====chapter #05=====
+// ====Question #10 =====
+// var bodmas=(5*10)/2;
+// document.write(bodmas);
+
+
+
+// ====Assignment#02 ====
+// =====chapter #05=====
+// ====Question #11 =====
+// document.write("<h1>Age Calculator</h1>")
+// var currentYear="2024";
+// var birthYear="2002";
+// var age=currentYear-birthYear;
+// document.write("Current Year: "+currentYear+"</br>"+"Birth Year: " +birthYear+"</br>")
+// document.write("Your Age is: "+age);
+
+
+
+// ====Assignment#02 ====
+// =====chapter #05=====
+// ====Question #12 =====
+// document.write("<h1>The Geometrizer</h1>")
+// var radius="20"
+// var circum=2*(3.142)*radius;
+// var area=3.142*(radius*radius);
+// document.write("Radius of a circle: "+radius+"</br>")
+// document.write("The Circumference is "+circum+"</br>");
+// document.write("The Area is "+area);
+
+
+
+
+// ====Assignment#02 ====
+// =====chapter #05=====
+// ====Question #13 =====
+// document.write("<h1>Life Time Calculator</h1>")
+// var age ="15"; 
+// var maxAge ="65";
+// var snacksPerDay ="3"; 
+
+// var yearsRemaining=maxAge-age;
+// var chocolatechip=yearsRemaining * 365 * snacksPerDay;
+// document.write("Favorite snacks: Chocolate Chip"+"</br>"+"Current Age: "+age+"</br>"+"Estimated Maximum:"+maxAge+"</br>"+"Amount snacks per day: "+snacksPerDay+"</br>");
+// document.write("You will need "+chocolatechip+" " +"chocolate chip to last you until the age of "+maxAge)
+
+
+// =========================== chapter #06 ================================
+
+// ====Assignment#02 ====
+// =====chapter #06=====
+// ====Question #1 =====
+// document.write("Result:"+"</br>"+"</br>");
+// var a=10;
+
+
+// document.write("The value of a is: "+a+"</br>");
+// document.write("---------------------------------------------"+"</br>");
+// ++a;
+// document.write("The value of a is: "+a+"</br>");
+// document.write("Now the value of a is: "+a+"</br>"+"</br>");
+
+
+// var y=a++;
+// document.write("The value of a is: "+y+"</br>");
+// document.write("Now the value of a is: "+a+"</br>"+"</br>");
+
+// --a;
+// document.write("The value of a is: "+a+"</br>");
+// document.write("Now the value of a is: "+a+"</br>"+"</br>");
+
+// var y=a--;
+// document.write("The value of a is: "+y+"</br>");
+// document.write("Now the value of a is: "+a+"</br>");
