ed10bf0342225362733ca239a37e2caac529af0e
Sameerbaacha <sameeris6512@gmail.com>
2024-03-04 02:05:26 +0500

    add Question 6 in app.js

--- app.js
+++ app.js
@@ -142,8 +142,8 @@
 // ====Assignment#02 ====
 // =====chapter #05=====
 // ====Question #05 =====
-// document.write("<h1>Table of 4</h1>")
-// var four="4";
+// document.write("<h1>Table of Any Number</h1>")
+// var four="5";
 // num="1";
 // var product=four*num;
 // document.write(four+"x"+num+"="+product+"</br>");
@@ -183,6 +183,19 @@
 // var product=four*num;
 // document.write(four+"x"+num+"="+product+"</br>");
 
+// document.write("<h1>Table of Any Number</h1>")
+// var table=10;
+// num=1;
+// document.write(table+"x" + num++ +"=" +table*1 +"</br>");
+// document.write(table+"x" + num++ +"=" +table*2 +"</br>");
+// document.write(table+"x" + num++ +"=" +table*3 +"</br>");
+// document.write(table+"x" + num++ +"=" +table*4+"</br>");
+// document.write(table+"x" + num++ +"=" +table*5+"</br>");
+// document.write(table+"x" + num++ +"=" +table*6+"</br>");
+// document.write(table+"x" + num++ +"=" +table*7+"</br>");
+// document.write(table+"x" + num++ +"=" +table*8+"</br>");
+// document.write(table+"x" + num++ +"=" +table*9+"</br>");
+// document.write(table+"x" + num++ +"=" +table*10+"</br>");
 
 
 // ====Assignment#02 ====
@@ -203,11 +216,11 @@
 // =====chapter #05=====
 // ====Question #07 =====
 // document.write("<h1>SHOPPING STORE</h1>")
-// var priceOfItem1="650";
-// var priceOfItem2="100";
-// var oderQuantityOfItem1="3";
-// var oderQuantityOfItem2="7";
-// var shippingCharges="100";
+// var priceOfItem1=650;
+// var priceOfItem2=100;
+// var oderQuantityOfItem1=3;
+// var oderQuantityOfItem2=7;
+// var shippingCharges=100;
 
 // var totalCostOfOrder=(priceOfItem1*oderQuantityOfItem1)+(priceOfItem2*oderQuantityOfItem2)+shippingCharges;
 // document.write("Price of item1 is "+priceOfItem1+"</br>"+"Oder Quantity of item1 is "+oderQuantityOfItem1+"</br>"+"Price of item2 is "+priceOfItem2+"</br>"+"Oder Quantity of item2 is "+oderQuantityOfItem1+"</br>"+"Shipping Charges is "+shippingCharges+"</br>"+"</br>")
@@ -312,3 +325,107 @@
 // var y=a--;
 // document.write("The value of a is: "+y+"</br>");
 // document.write("Now the value of a is: "+a+"</br>");
+
+
+// ====Assignment#02 ====
+// =====chapter #06=====
+// ====Question #02 =====
+// var a=2;
+// var b=1;
+// var result=--a;
+// document.write("a is "+a +"</br>");
+// document.write("b is "+b +"</br>");
+// document.write("result is "+result+"</br>"+"</br>");
+// var result=--a - --b;
+// document.write("a is "+a +"</br>");
+// document.write("b is "+b +"</br>");
+// document.write("result is "+result+"</br>"+"</br>");
+// var result=--a - --b + ++b;
+// document.write("a is "+a +"</br>");
+// document.write("b is "+b +"</br>");
+// document.write("result is "+result+"</br>"+"</br>");
+// var result=--a - --b + ++b + b--;
+// document.write("a is "+a +"</br>");
+// document.write("b is "+b +"</br>");
+// document.write("result is "+result);
+
+// ====Assignment#02 ====
+// =====chapter #06=====
+// ====Question #03 =====
+// var naam= prompt("enter your name");
+// document.write("Hello "+ naam + "how are you");
+
+// ====Assignment#02 ====
+// =====chapter #06=====
+// ====Question #05 =====
+// var table =+prompt("Enter a number");
+// if(table===0) {
+//     table=5;
+// }
+// document.write("<h2>Multiplication table of "+table+"</h2>");
+// document.write(table + " x 1 = " + table * 1 + "</br>");
+// document.write(table + " x 2 = " + table * 2 + "</br>");
+// document.write(table + " x 3 = " + table * 3 + "</br>");
+// document.write(table + " x 4 = " + table * 4 + "</br>");
+// document.write(table + " x 5 = " + table * 5 + "</br>");
+// document.write(table + " x 6 = " + table * 6 + "</br>");
+// document.write(table + " x 7 = " + table * 7 + "</br>");
+// document.write(table + " x 8 = " + table * 8 + "</br>");
+// document.write(table + " x 9 = " + table * 9 + "</br>");
+// document.write(table + " x 10 = " + table * 10);
+
+// ====Assignment#02 ====
+// =====chapter #06=====
+// ====Question #06 =====
+// a) Take three subjects names from the user and store them in 3 different variables.
+// var subject1 = prompt("Enter name of first subject:");
+// var subject2 = prompt("Enter name of second subject:");
+// var subject3 = prompt("Enter name of third subject:");
+
+// // b) Total marks for each subject is 100, store it in another variable.
+// var totalMarks = 100;
+
+// // c) Take obtained marks for the first subject from the user and store it in a different variable.
+// var obtainedMarks1 = parseInt(prompt("Enter obtained marks for " + subject1 + ":"));
+
+// // d) Take obtained marks for the remaining 2 subjects from the user and store them in variables.
+// var obtainedMarks2 = parseInt(prompt("Enter obtained marks for " + subject2 + ":"));
+// var obtainedMarks3 = parseInt(prompt("Enter obtained marks for " + subject3 + ":"));
+
+// // e) Now calculate total marks and percentage and show the result in the browser
+// var totalObtainedMarks = obtainedMarks1 + obtainedMarks2 + obtainedMarks3;
+// var totalPercentage = (totalObtainedMarks / (totalMarks * 3)) * 100;
+
+// // Displaying the result in a table format
+// document.write("<h2>Result</h2>");
+// document.write("<table border='1'>");
+// document.write("<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th></tr>");
+// document.write("<tr><td>" + subject1 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks1 + "</td></tr>");
+// document.write("<tr><td>" + subject2 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks2 + "</td></tr>");
+// document.write("<tr><td>" + subject3 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks3 + "</td></tr>");
+// document.write("<tr><td colspan='3'>Total Obtained Marks: " + totalObtainedMarks + "</td></tr>");
+// document.write("<tr><td colspan='3'>Percentage: " + totalPercentage.toFixed(2) + "%</td></tr>");
+// document.write("</table>");
+
+var subject1= prompt("Enter First subject name");
+var subject2= prompt("Enter Second subject name");
+var subject3= prompt("Enter Third subject name");
+var totalMarks=100;
+var obtMarks1=+prompt("Enter obtained marks for "+subject1);
+var obtMarks2=+prompt("Enter obtained marks for "+subject2);
+var obtMarks3=+prompt("Enter obtained marks for "+subject3);
+var percentage1= obtMarks1/totalMarks*100;
+var percentage2= obtMarks2/totalMarks*100;
+var percentage3= obtMarks3/totalMarks*100;
+var obtainedMarks= obtMarks1 + obtMarks2 + obtMarks3;
+var percentage= (obtainedMarks/(totalMarks*3))*100;
+
+document.write("<h2>Result</h2>");
+
+document.write("<table border='2'>");
+document.write("<tr> <th>Subject</th> <th>Total Marks</th> <th>Obtained Marks</th> <th>Percentage</th> </tr>");
+document.write("<tr> <td>"+subject1+"</td> <td>"+totalMarks+"</td> <td>"+obtMarks1+"</td> <td>"+percentage1+"%"+"</td></tr>");
+document.write("<tr> <td>"+subject2+"</td> <td>"+totalMarks+"</td> <td>"+obtMarks2+"</td> <td>"+percentage2+"%"+"</td></tr>");
+document.write("<tr> <td>"+subject3+"</td> <td>"+totalMarks+"</td> <td>"+obtMarks3+"</td><td>"+percentage3+"%"+"</td></tr>");
+document.write("<tr> <td>Total Marks "+totalMarks*3+"</td> <td> Obtained marks "+obtainedMarks+"</td> <td colspan='3'>Percentage " +percentage+"%"+"</td></tr>");
+document.write("</table>");
